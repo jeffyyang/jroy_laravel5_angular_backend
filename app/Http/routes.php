d173e042e63b2faebb4a5ea6bd093f256990eb48
jroy <jroy@foxmail.com>
2015-03-07 13:51:34 +0800

    remove backend module

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,3 +19,58 @@ Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
 ]);
+
+/*
+|--------------------------------------------------------------------------
+| Backend
+|--------------------------------------------------------------------------
+|
+| 后台基于AngularJs，无需单独模块
+|
+*/
+Route::controller('backend/auth', 'Backend\AuthController');
+
+Route::get('/backend',['as'=>'backend', function(){
+    return view('backend');
+}]);
+
+Route::group(['prefix' => 'backend', 'namespace' => 'Backend', 'middleware'=>'admin'], function()
+{
+	Route::controllers([
+		'setting'	=> 'SettingController',
+	]);
+	
+	//会员管理
+    Route::controller('user', 'UserController', [
+        'getIndex'      => 'backend.user.index',
+        'getEdit'       => 'backend.user.edit',
+        'postStore'     => 'backend.user.create',
+        'putUpdate'     => 'backend.user.update',
+        'deleteDestroy' => 'backend.user.delete'
+    ]);
+    //用户组管理
+    Route::controller('role', 'RoleController', [
+        'getIndex'      => 'backend.role.index',
+        'getEdit'       => 'backend.role.edit',
+        'postStore'     => 'backend.role.create',
+        'putUpdate'     => 'backend.role.update',
+        'deleteDestroy' => 'backend.role.delete',
+        'getAccess'     => 'backend.role.access'
+    ]);
+
+    //cms内管理
+    Route::controller('cms', 'CmsController', [
+        'getIndex'       => 'backend.cms.post.index',
+        'getEdit'        => 'backend.cms.post.edit',
+        'postStore'      => 'backend.cms.post.create',
+        'putUpdate'      => 'backend.cms.post.update',
+        'deleteDestroy'  => 'backend.cms.post.delete',
+        //栏目
+        'getAllCate'        =>'backend.cms.allcate',
+        'getListCate'        =>'backend.cms.listcate',
+        'getEditCate'       => 'backend.cms.category.edit',
+        'postStoreCate'     => 'backend.cms.category.create',
+        'putUpdateCate'     => 'backend.cms.category.update',
+        'deleteDestroyCate' => 'backend.cms.category.delete',
+    ]);
+});
